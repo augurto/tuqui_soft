f40d2898a0563f7450368f0a119b781d1c701c7c
Ego <augurto.17@gmail.com>
2023-04-18 02:42:13 -0500

    carga de inico de sesion

--- cliente.php
+++ cliente.php
@@ -4,11 +4,18 @@ session_start();
 
 // Verificar si existe la variable de sesión tipo_user y si es igual a 3
 if (isset($_SESSION['tipo_user']) && $_SESSION['tipo_user'] == 3) {
-  // El usuario es un administrador, se puede mostrar el contenido
-  echo "Bienvenido, Usuario";
+  // El usuario es un cliente, se puede mostrar el contenido
+  $nombre = $_SESSION['tipo_user'];
+
 } else {
-  // Si no es un administrador, redirigir a la página de inicio
+  // Si no es un cliente, redirigir a la página de inicio
   header("Location: login/index.php");
   exit();
 }
 ?>
+
+<!-- Dentro del formulario puedes imprimir los valores en inputs -->
+
+<input type="text" name="nombre" value="<?php echo $nombre; ?>">
+
+
