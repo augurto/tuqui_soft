7b8a8b3fcfd2bd064512d5eafa13cfb8466b8e59
Ego <augurto.17@gmail.com>
2023-04-18 03:19:04 -0500

    CREACION DE PARTES

---
+++ includes/inferior.php
@@ -0,0 +1,46 @@
+</div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="plantilla/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="plantilla/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="plantilla/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="plantilla/dist/js/adminlte.js"></script>
+
+
+<!-- PAGE plantilla/plugins -->
+<!-- jQuery Mapael -->
+<script src="plantilla/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="plantilla/plugins/raphael/raphael.min.js"></script>
+<script src="plantilla/plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="plantilla/plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<!-- ChartJS -->
+<script src="plantilla/plugins/chart.js/Chart.min.js"></script>
+
+<!-- AdminLTE for demo purposes -->
+<script src="plantilla/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="plantilla/dist/js/pages/dashboard2.js"></script>
+</body>
+</html>
