9196ab05fe68a1e3d442e1441d26992b2d0012ca
Ego <augurto.17@gmail.com>
2023-04-19 03:47:20 -0500

    archivo proyecto creado

--- administrador.php
+++ administrador.php
@@ -42,6 +42,25 @@ mysqli_close($conn);
 include_once './includes/superior.php';
 ?>
 <!-- Insertar codigo al Body -->
+ <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Panel de Administracion</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Proyectos</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
 
 <div class="row">
           <div class="col-12 col-sm-6 col-md-3">
--- includes/superior.php
+++ includes/superior.php
@@ -25,28 +25,6 @@
   <?php include 'navbar.php'; ?>
   <?php include 'menu.php'; ?>
 
- 
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Panel de Administracion</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Proyectos</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
     <!-- INICIO CODIGO -->
 
   
