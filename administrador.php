abe20863d50ff166cdb3db23e8af3186e3c644b5
Ego <augurto.17@gmail.com>
2023-04-19 03:28:07 -0500

    cambio de iconos

--- administrador.php
+++ administrador.php
@@ -46,7 +46,7 @@ include_once './includes/superior.php';
 <div class="row">
           <div class="col-12 col-sm-6 col-md-3">
             <div class="info-box">
-              <span class="info-box-icon bg-info elevation-1"><i class="fa-regular fa-file"></i></span>
+              <span class="info-box-icon bg-info elevation-1"><i class="fas fa-parking"></i></i></span>
 
               <div class="info-box-content">
                 <span class="info-box-text">Proyectos</span>
@@ -79,11 +79,11 @@ include_once './includes/superior.php';
 
           <div class="col-12 col-sm-6 col-md-3">
             <div class="info-box mb-3">
-              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-money-check-edit-alt"></i></span>
 
               <div class="info-box-content">
                 <span class="info-box-text">Contabilidad</span>
-                <span class="info-box-number">760</span>
+                <span class="info-box-number">S/.760</span>
               </div>
               <!-- /.info-box-content -->
             </div>
