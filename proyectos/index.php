1ec63fba15b45888949f0bbe6ff3eedbbac0b773
Ego <augurto.17@gmail.com>
2023-04-19 03:49:45 -0500

    superior.php

--- proyectos/index.php
+++ proyectos/index.php
@@ -1,4 +1,26 @@
  
+
+<?php
+session_start();
+
+// Verificar si existe la variable de sesión tipo_user y si es igual a 0
+if (isset($_SESSION['tipo_user']) && $_SESSION['tipo_user'] == 0) {
+  // El usuario es un administrador, se puede mostrar el contenido
+  $tipo_usuario = $_SESSION['tipo_user'];
+  
+  echo "Bienvenido, Adminstrador";
+} else {
+  // Si no es un administrador, redirigir a la página de inicio
+  header("Location: login/index.php");
+  exit();
+}
+?>
+<input type="hidden" name="nombre" value="<?php echo $tipo_usuario; ?>">
+<?php
+include_once './includes/superior.php';
+?>
+<!-- Insertar codigo al Body -->
+
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -580,3 +602,8 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
+
+  <!-- FIN codigo BODY -->
+<?php
+include_once "./includes/inferior.php";
+?>
