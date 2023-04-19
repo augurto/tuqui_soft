5a05ea45adaf060c7a88cb2fac412b066bdad253
Ego <augurto.17@gmail.com>
2023-04-19 03:30:00 -0500

    cambio de titulo a general

--- administrador.php
+++ administrador.php
@@ -79,7 +79,7 @@ include_once './includes/superior.php';
 
           <div class="col-12 col-sm-6 col-md-3">
             <div class="info-box mb-3">
-              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-money-check-edit-alt"></i></span>
+              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-money-check-alt"></i></span>
 
               <div class="info-box-content">
                 <span class="info-box-text">Contabilidad</span>
