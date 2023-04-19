4317bd9141d9022d7e4f90fa574aff342edb50bc
Ego <augurto.17@gmail.com>
2023-04-19 03:21:29 -0500

    cambio de icono y nombre de proyectos

--- administrador.php
+++ administrador.php
@@ -46,13 +46,13 @@ include_once './includes/superior.php';
 <div class="row">
           <div class="col-12 col-sm-6 col-md-3">
             <div class="info-box">
-              <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
+              <span class="info-box-icon bg-info elevation-1"><i class="fa-duotone fa-rectangle-history-circle-plus"></i></span>
 
               <div class="info-box-content">
-                <span class="info-box-text">CPU Traffic</span>
+                <span class="info-box-text">Proyectos</span>
                 <span class="info-box-number">
                   10
-                  <small>%</small>
+                  
                 </span>
               </div>
               <!-- /.info-box-content -->
