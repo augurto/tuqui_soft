475585b224debb77e27ba779f7cbd4bac624911a
Ego <augurto.17@gmail.com>
2023-04-19 23:29:01 -0500

    prueba 5

--- includes/modal/agregarProyecto.php
+++ includes/modal/agregarProyecto.php
@@ -26,33 +26,37 @@
                 <!-- FIN CLIENTE -->
             
               <!-- ASIGNAR USUARIOS AL PROYECTO -->
-              <div class="form-group">
-                <label>Asignar asesor</label>
-                <div class="select2-purple" style="display: flex;">
-                    <select id="asesor-select" class="select2" data-placeholder="Select a State" data-dropdown-css-class="select2-purple" style="width: 100%;">
-                        <?php
-                            $query = "SELECT id, nombre, rol FROM usuarios WHERE rol != 3";
-                            $result = mysqli_query($conn, $query);
-                            while ($row = mysqli_fetch_assoc($result)) {
-                        ?>
-                            <option value="<?php echo $row['id']; ?>"><?php echo $row['nombre']; ?></option>
-                        <?php } ?>
-                    </select>
-                    <button id="agregar-asesor" class="form-control" type="button" style="margin-left: 10px;">Agregar asesor</button>
+                <div class="form-group">
+                    <label>Asignar asesor</label>
+                    <div class="select2-purple" style="display: flex;">
+                        <select id="asesor-select" class="select2" data-placeholder="Select a State" data-dropdown-css-class="select2-purple" style="width: 100%;">
+                            <?php
+                                $query = "SELECT id, nombre, rol FROM usuarios WHERE rol != 3";
+                                $result = mysqli_query($conn, $query);
+                                while ($row = mysqli_fetch_assoc($result)) {
+                            ?>
+                                <option value="<?php echo $row['id']; ?>"><?php echo $row['nombre']; ?></option>
+                            <?php } ?>
+                        </select>
+                        <select id="rol-select" class="select2" data-placeholder="Selecciona Rol" data-dropdown-css-class="select2-purple" style="width: 100%;">
+                            <option value="coordinador">Coordinador</option>
+                            <option value="redactor">Redactor</option>
+                        </select>
+                        <button id="agregar-asesor" class="form-control" type="button" style="margin-left: 10px;">Agregar asesor</button>
+                    </div>
                 </div>
-            </div>
 
-            <table id="tabla-asesores" class="table table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Acción</th>
-                    </tr>
-                </thead>
-                <tbody>
-                </tbody>
-            </table>
-
+                <table id="tabla-asesores" class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Rol</th>
+                            <th>Acción</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
                 
               
               <!-- FIN USUARIOS AL PROYECTO -->
@@ -92,35 +96,37 @@
       <!-- /.modal -->
 
       <!-- INICIO DE SCRIPT -->
-   <script>
-    // Seleccionamos el botón y el select
-    const agregarAsesorBtn = document.getElementById('agregar-asesor');
-    const asesorSelect = document.getElementById('asesor-select');
+    <script>
+        // Seleccionamos el botón y los selects
+        const agregarAsesorBtn = document.getElementById('agregar-asesor');
+        const asesorSelect = document.getElementById('asesor-select');
+        const rolSelect = document.getElementById('rol-select');
 
-    // Seleccionamos la tabla donde vamos a agregar los asesores
-    const tablaAsesores = document.getElementById('tabla-asesores');
+        // Seleccionamos la tabla donde vamos a agregar los asesores
+        const tablaAsesores = document.getElementById('tabla-asesores');
 
-    // Agregamos un listener al botón
-    agregarAsesorBtn.addEventListener('click', function() {
-    // Obtenemos el valor del select
-    const asesorId = asesorSelect.value;
-    const asesorNombre = asesorSelect.options[asesorSelect.selectedIndex].text;
+        // Agregamos un listener al botón
+        agregarAsesorBtn.addEventListener('click', function() {
+            // Obtenemos el valor de los selects
+            const asesorId = asesorSelect.value;
+            const asesorNombre = asesorSelect.options[asesorSelect.selectedIndex].text;
+            const rol = rolSelect.value;
 
-    // Creamos una nueva fila en la tabla con el nombre del asesor y un botón para eliminarlo
-    const nuevaFila = document.createElement('tr');
-    nuevaFila.innerHTML = `
-        <td>${asesorNombre}</td>
-        <td><button class="btn btn-danger btn-sm eliminar-asesor">Eliminar</button></td>
-    `;
+            // Creamos una nueva fila en la tabla con el nombre del asesor, el rol y un botón para eliminarlo
+            const nuevaFila = document.createElement('tr');
+            nuevaFila.innerHTML = `
+                <td>${asesorNombre}</td>
+                <td>${rol}</td>
+                <td><button class="btn btn-danger btn-sm eliminar-asesor">Eliminar</button></td>
+            `;
 
-    // Agregamos la nueva fila a la tabla
-    tablaAsesores.querySelector('tbody').appendChild(nuevaFila);
+            // Agregamos la nueva fila a la tabla
+            tablaAsesores.querySelector('tbody').appendChild(nuevaFila);
 
-    // Agregamos un listener al botón de eliminar para eliminar la fila correspondiente
-    const eliminarAsesorBtn = nuevaFila.querySelector('.eliminar-asesor');
-    eliminarAsesorBtn.addEventListener('click', function() {
-        nuevaFila.remove();
-    });
-    });
-
-   </script>
+            // Agregamos un listener al botón de eliminar para eliminar la fila correspondiente
+            const eliminarAsesorBtn = nuevaFila.querySelector('.eliminar-asesor');
+            eliminarAsesorBtn.addEventListener('click', function() {
+                nuevaFila.remove();
+            });
+        });
+    </script>
