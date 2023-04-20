85cbbc036cb41d3658c249458ade85faf3276c27
Ego <augurto.17@gmail.com>
2023-04-20 00:30:37 -0500

    cambio de id al boton

--- includes/modal/agregarProyecto.php
+++ includes/modal/agregarProyecto.php
@@ -86,7 +86,9 @@
             </div>
             <div class="modal-footer justify-content-between">
               <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-              <button type="button" class="btn btn-primary" onclick="guardarProyecto()">Guardar</button>
+              
+              <button id="guardar-proyecto" type="button" class="btn btn-primary">Guardar</button>
+
 
             </div>
           </div>
