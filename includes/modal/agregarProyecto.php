692cae00e6eb798545534596fcb656ac60229242
Ego <augurto.17@gmail.com>
2023-04-19 21:40:00 -0500

    mosntrando parametro de agergar proyecto

--- includes/modal/agregarProyecto.php
+++ includes/modal/agregarProyecto.php
@@ -8,7 +8,65 @@
               </button>
             </div>
             <div class="modal-body">
-              <p>One fine body&hellip;</p>
+                <!-- NOMBRE DEL PROYECTO -->
+                <div class="form-group">
+                    <label>Nombre del proyecto</label>
+                    <input type="text" class="form-control" placeholder="Ingrese el nombre del proyecto">
+                </div>
+
+                <!-- SELECCIONAR CLIENTE  -->
+                <div class="form-group">
+                <label>Cliente</label>
+                <select class="form-control select2" style="width: 100%;">
+                    <?php while ($fila = mysqli_fetch_array($resultado)) { ?>
+                    <option value="<?php echo $fila['id']; ?>"><?php echo $fila['nombre']; ?></option>
+                    <?php } ?>
+                </select>
+                </div>
+                <!-- FIN CLIENTE -->
+            
+              <!-- ASIGNAR USUARIOS AL PROYECTO -->
+              
+                <div class="form-group">
+                  <label>Asignar usuarios</label>
+                  <div class="select2-purple">
+                    <select class="select2" multiple="multiple" data-placeholder="Select a State" data-dropdown-css-class="select2-purple" style="width: 100%;">
+                      <option>Alabama</option>
+                      <option>Alaska</option>
+                      <option>California</option>
+                      <option>Delaware</option>
+                      <option>Tennessee</option>
+                      <option>Texas</option>
+                      <option>Washington</option>
+                    </select>
+                  </div>
+                </div>
+                <!-- /.form-group -->
+              
+              <!-- FIN USUARIOS AL PROYECTO -->
+
+              <!-- FECHA DE ENTREGA -->
+                <div class="form-group">
+                <label>Fecha de entrega</label>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
+                    </div>
+                    <input type="text" class="form-control float-right" id="reservation">
+                </div>
+                </div>
+
+                <!-- MONTO -->
+                <div class="form-group">
+                <label>Monto</label>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-dollar-sign"></i></span>
+                    </div>
+                    <input type="text" class="form-control">
+                </div>
+                </div>
+                <!-- FIN MONTO -->
             </div>
             <div class="modal-footer justify-content-between">
               <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
